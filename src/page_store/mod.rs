46d5383d4c95df011b37c018fda924e344a86f68
Michael Zehender <michael@beamic.eu>
2019-12-20 09:46:02 +0100

    implement in-memory bitmap index page that can be persisted

--- src/page_store/mod.rs
+++ src/page_store/mod.rs
@@ -3,6 +3,7 @@ use std::io::{Seek, Write, SeekFrom};
 use std::io::{Error, ErrorKind, Result};
 use memmap::{Mmap, MmapOptions};
 use std::error;
+use std::sync::Arc;
 
 #[cfg(test)]
 mod tests;
@@ -11,17 +12,18 @@ const PAGE_SIZE: usize = 4096;
 
 pub struct PageStore {
     file: File,
-    mmap: Mmap,
-    max_size: usize,
+    mmap: Arc<Mmap>,
+    pub(crate) max_size: usize,
     pub(crate) current_size: usize,
 }
 
 impl PageStore {
     pub fn new(file: File, max_size: usize) -> Result<PageStore> {
         let current_size = file.metadata()?.len() as usize;
-        let mmap = unsafe {
-           MmapOptions::new().len(max_size).map(&file)?
+        let mem = unsafe {
+            MmapOptions::new().len(max_size).map(&file)?
         };
+        let mmap = Arc::new(mem);
         Ok(PageStore { file, mmap, max_size, current_size })
     }
 
@@ -30,7 +32,7 @@ impl PageStore {
         self.file.sync_data()
     }
 
-    pub fn read_page(&self, id: usize) -> Result<&[u8]> {
+    pub fn read_page(&self, id: usize) -> Result<MemoryPage> {
         let offset = id * PAGE_SIZE;
         let end = offset + PAGE_SIZE;
         if end > self.current_size {
@@ -42,7 +44,7 @@ impl PageStore {
                 }
             );
         }
-        Ok(&self.mmap[offset..end])
+        Ok(MemoryPage { start: offset, end, mmap: self.mmap.clone() })
     }
 
     pub fn write_page(&mut self, id: usize, buf: &[u8]) -> Result<()> {
@@ -84,6 +86,192 @@ impl PageStore {
         Ok(())
     }
 }
+
+pub struct MemoryPage {
+    start: usize,
+    end: usize,
+    mmap: Arc<Mmap>,
+}
+
+impl<'a> MemoryPage {
+    pub fn page_id(&self) -> u32 {
+        self.extract_u32(0)
+    }
+
+    pub fn page_type(&self) -> u32 {
+        self.extract_u32(4)
+    }
+
+    pub fn extract_u32(&self, idx: usize) -> u32 {
+        let s = &self.content()[idx..idx + 4];
+        let mut a: [u8; 4] = [0; 4];
+        a.copy_from_slice(s);
+
+        u32::from_le_bytes(a)
+    }
+
+    pub fn extract_u16(&self, idx: usize) -> u16 {
+        let s = &self.content()[idx..idx + 2];
+        let mut a: [u8; 2] = [0; 2];
+        a.copy_from_slice(s);
+
+        u16::from_le_bytes(a)
+    }
+
+    pub fn content(&'a self) -> &'a [u8] {
+        &self.mmap[self.start..self.end]
+    }
+}
+
+const BITMAP_INDEX_PAGE_TYPE : u32 = 1;
+const BITMAP_INDEX_PAGE_HEADER_SIZE: usize = 16;
+const BITMAP_INDEX_PAGE_COUNT: u16 = ((PAGE_SIZE - BITMAP_INDEX_PAGE_HEADER_SIZE) * 8) as u16;
+
+pub struct BitmapAllocationPage {
+    page_id: u32,
+    first_managed_page_id: u32,
+    last_managed_page_id: u32,
+    current_first_free_page_idx: u16,
+    first_free_page_idx: u16,
+    free_page_count: u16,
+    buffer: [u8; PAGE_SIZE],
+}
+
+impl<'a> BitmapAllocationPage {
+    pub fn new(page_id: u32, first_managed_page_id: u32) -> BitmapAllocationPage {
+        let last_managed_page_id = first_managed_page_id + (BITMAP_INDEX_PAGE_COUNT as u32) - 1;
+
+        let mut page = BitmapAllocationPage {
+            page_id,
+            first_managed_page_id,
+            last_managed_page_id,
+            current_first_free_page_idx: 0,
+            first_free_page_idx: 0,
+            free_page_count: BITMAP_INDEX_PAGE_COUNT,
+            buffer: [0; PAGE_SIZE],
+        };
+        if page_id >= first_managed_page_id && page_id <= last_managed_page_id {
+            page.mark_used(page_id)
+        }
+        page
+    }
+
+
+    pub fn allocate(&mut self, f: fn(u32) -> bool) -> Option<u32> {
+        while self.current_first_free_page_idx != 0xFFFF {
+            let candidate = self.first_managed_page_id + self.current_first_free_page_idx as u32;
+            if f(candidate) {
+                self.mark_used(candidate);
+
+                return Some(candidate)
+            } else {
+                self.current_first_free_page_idx = self.find_next_free_page_index(self.current_first_free_page_idx+1)
+            }
+        }
+        None
+    }
+
+    fn mark_used(&mut self, page_id: u32) {
+        let offset = page_id - self.first_managed_page_id;
+        let byte_index = BITMAP_INDEX_PAGE_HEADER_SIZE + (offset as usize >> 3);
+        let bit: u8 = (1 << (offset & 0x07)) as u8;
+
+        let bitmap = self.bitmap();
+        if bitmap[byte_index] & bit == 0 {
+            bitmap[byte_index] |= bit;
+
+            self.free_page_count -= 1;
+            if page_id == (self.first_managed_page_id + self.current_first_free_page_idx as u32) {
+                let next = self.find_next_free_page_index(self.current_first_free_page_idx + 1);
+                if self.first_free_page_idx == self.current_first_free_page_idx {
+                    self.first_free_page_idx = next;
+                }
+                self.current_first_free_page_idx = next;
+            }
+        }
+    }
+
+    fn find_next_free_page_index(&mut self, start: u16) -> u16 {
+        let bitmap = self.bitmap();
+        let byte_start_index = BITMAP_INDEX_PAGE_HEADER_SIZE + (start >> 3) as usize;
+
+        for byte_index in byte_start_index..PAGE_SIZE {
+            let byte = bitmap[byte_index];
+            if byte != 0xFF {
+                for bit in 0..7 as u16 {
+                    let mask = (1 << bit) as u8;
+                    if byte & mask == 0 {
+                        let candidate = (((byte_index - BITMAP_INDEX_PAGE_HEADER_SIZE) as u16) << 3) + bit;
+                        if candidate >= start {
+                            return candidate;
+                        }
+                    }
+                }
+            }
+        }
+
+        0xFFFF
+    }
+
+
+    pub fn free(&mut self, page_id: u32) -> bool {
+        if page_id >= self.first_managed_page_id && page_id <= self.last_managed_page_id {
+            self.mark_free(page_id);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn mark_free(&mut self, page_id: u32) {
+        let offset = page_id - self.first_managed_page_id;
+        let byte_index = BITMAP_INDEX_PAGE_HEADER_SIZE + (offset as usize >> 3);
+        let bit: u8 = (1 << (offset & 0x07)) as u8;
+        let mask: u8 = !bit;
+
+
+        let bitmap = self.bitmap();
+        if bitmap[byte_index] & bit == bit {
+            bitmap[byte_index] &= mask;
+
+            self.free_page_count += 1;
+            if page_id < (self.first_managed_page_id + self.first_free_page_idx as u32) {
+                self.first_free_page_idx = (page_id - self.first_managed_page_id) as u16
+            }
+        }
+    }
+
+
+    fn bitmap(&'a mut self) -> &'a mut [u8] {
+        &mut self.buffer[BITMAP_INDEX_PAGE_HEADER_SIZE..PAGE_SIZE]
+    }
+
+
+    pub fn persist(mut self, store: &mut PageStore) -> Result<()> {
+        self.update_header();
+
+        store.write_page(self.page_id as usize, &self.buffer)
+    }
+
+    fn update_header(&mut self) {
+        let page_id_bytes = self.page_id.to_le_bytes();
+        self.buffer[0..4].clone_from_slice(&page_id_bytes);
+
+        let page_type_bytes = BITMAP_INDEX_PAGE_TYPE.to_le_bytes();
+        self.buffer[4..8].clone_from_slice(&page_type_bytes);
+
+        let first_page_id_bytes = self.first_managed_page_id.to_le_bytes();
+        self.buffer[8..12].clone_from_slice(&first_page_id_bytes);
+
+        let free_page_count_bytes = self.free_page_count.to_le_bytes();
+        self.buffer[12..14].clone_from_slice(&free_page_count_bytes);
+
+        let free_page_index_bytes = self.first_free_page_idx.to_le_bytes();
+        self.buffer[14..16].clone_from_slice(&free_page_index_bytes);
+    }
+}
+
+
 
 fn invalid_input<T, E>(message: E) -> Result<T>
     where E: Into<Box<dyn error::Error + Send + Sync>>
--- src/page_store/tests.rs
+++ src/page_store/tests.rs
@@ -1,11 +1,11 @@
-use crate::page_store::{PageStore, PAGE_SIZE};
+use crate::page_store::{PageStore, PAGE_SIZE, BitmapAllocationPage, BITMAP_INDEX_PAGE_COUNT, BITMAP_INDEX_PAGE_TYPE};
 
 use std::error::Error;
 use std::fs::File;
 use std::io::Result;
 use tempfile::tempfile;
 
-const TESTDB_MAX_SIZE: usize = 16384;
+const TESTDB_MAX_SIZE: usize = 163840;
 
 #[test]
 fn buffer_too_small() {
@@ -74,6 +74,19 @@ fn range_out_of_bounds() {
 }
 
 #[test]
+fn write_after_last_page() {
+    let vec: Vec<u8> = vec![0; 256];
+
+    let file = tempfile().unwrap();
+    let mut store = PageStore::new(file, TESTDB_MAX_SIZE).unwrap();
+
+    match store.write_page_range((TESTDB_MAX_SIZE / PAGE_SIZE) + 1, 0, &vec) {
+        Err(e) => (),
+        Ok(()) => panic!("should have failed to write page subset")
+    }
+}
+
+#[test]
 fn writes_first_page_range_start() {
     let vec: Vec<u8> = vec![0; 256];
 
@@ -121,6 +134,80 @@ fn read_back_page() {
     store.write_page_range(0, 0, &vec).unwrap();
     let page = store.read_page(0).unwrap();
 
-    assert_eq!(&vec[0..5], &page[0..5]);
-    assert_eq!(0 as u8, page[PAGE_SIZE - 1])
+    assert_eq!(&vec[0..5], &page.content()[0..5]);
+    assert_eq!(0 as u8, page.content()[PAGE_SIZE - 1])
+}
+
+#[test]
+fn new_allocator_for_new_database() {
+    let file = tempfile().unwrap();
+    let mut store = PageStore::new(file, TESTDB_MAX_SIZE);
+
+    let mut page = BitmapAllocationPage::new(2, 0);
+    page.mark_used(0);
+    page.mark_used(1);
+
+    assert_eq!(2, page.page_id);
+    assert_eq!(0, page.first_managed_page_id);
+    assert_eq!(3, page.first_free_page_idx);
+    assert_eq!(BITMAP_INDEX_PAGE_COUNT - 3, page.free_page_count);
+}
+
+#[test]
+fn new_allocator_for_existing_database() {
+    let file = tempfile().unwrap();
+    let mut store = PageStore::new(file, TESTDB_MAX_SIZE);
+
+    let mut page = BitmapAllocationPage::new(2, 10);
+
+    assert_eq!(2, page.page_id);
+    assert_eq!(10, page.first_managed_page_id);
+    assert_eq!(0, page.first_free_page_idx);
+    assert_eq!(BITMAP_INDEX_PAGE_COUNT, page.free_page_count);
+}
+
+
+#[test]
+fn allocator_allocates_pages_monotonically_increasing() {
+    let mut page = BitmapAllocationPage::new(2, 10);
+    let f = |_: u32| true;
+
+    assert_eq!(Some(10), page.allocate(f));
+    assert_eq!(Some(11), page.allocate(f));
+    assert_eq!(Some(12), page.allocate(f));
+    assert_eq!(true, page.free(11));
+    assert_eq!(Some(13), page.allocate(f));
+}
+
+
+#[test]
+fn allocator_allocates_pages_monotonically_increasing_and_skips_used_pages() {
+    let mut page = BitmapAllocationPage::new(2, 10);
+
+    let f = |x: u32| x != 11 && x != 12 && x != 14;
+
+    assert_eq!(Some(10), page.allocate(f));
+    assert_eq!(Some(13), page.allocate(f));
+    assert_eq!(Some(15), page.allocate(f));
+    assert_eq!(true, page.free(13));
+    assert_eq!(Some(16), page.allocate(f));
+}
+
+
+#[test]
+fn persist_writes_correct_index() {
+    let file = tempfile().unwrap();
+    let mut store = PageStore::new(file, TESTDB_MAX_SIZE).unwrap();
+    let mut page = BitmapAllocationPage::new(2, 0);
+    page.mark_used(0);
+    page.mark_used(1);
+
+    page.persist(&mut store).unwrap();
+
+    let memory_page = store.read_page(2).unwrap();
+    assert_eq!(2, memory_page.page_id());
+    assert_eq!(BITMAP_INDEX_PAGE_TYPE, memory_page.page_type());
+    assert_eq!(0, memory_page.extract_u32(8)); // first_managed_page_id
+    assert_eq!(BITMAP_INDEX_PAGE_COUNT - 3, memory_page.extract_u16(12)); // free page count
+    assert_eq!(3, memory_page.extract_u16(14)); // free page count
 }
